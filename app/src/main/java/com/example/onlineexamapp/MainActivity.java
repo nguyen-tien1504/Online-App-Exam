3d6bd36e1be65453720ff3f95d02df5bdecf68ff
nguyen-tien1504 <tien1572@gmail.com>
2025-05-27 20:14:29 +0700

    add session

--- app/src/main/java/com/example/onlineexamapp/MainActivity.java
+++ app/src/main/java/com/example/onlineexamapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.onlineexamapp;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,6 +19,18 @@ import models.User;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    // creating constant keys for shared preferences.
+    public static final String SHARED_PREFS = "shared_prefs";
+
+    // key for storing email.
+    public static final String EMAIL_KEY = "email_key";
+
+    // key for storing password.
+    public static final String PASSWORD_KEY = "password_key";
+
+    // variable for shared preferences.
+    SharedPreferences sharedpreferences;
+    String emailShare, passwordShare;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,6 +52,16 @@ public class MainActivity extends AppCompatActivity {
 //            finish();
 //        });
 
+        sharedpreferences = getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+        emailShare = sharedpreferences.getString(EMAIL_KEY, null);
+        passwordShare = sharedpreferences.getString(PASSWORD_KEY, null);
+
+        // check if the fields are not null then one current user is loggedIn
+        if (email != null && password != null) {
+            Intent i = new Intent(MainActivity.this, Home.class);
+            startActivity(i);
+            finish();
+        }
         binding.signup.setOnClickListener(view->{
             Intent myIntent = new Intent(MainActivity.this, SignUp.class);
             startActivity(myIntent);
@@ -50,6 +74,16 @@ public class MainActivity extends AppCompatActivity {
             if(findUser == null){
                 errMsg.setText("Wrong email or password!!! ");
             }else{
+                SharedPreferences.Editor editor = sharedpreferences.edit();
+
+                // below two lines will put values for
+                // email and password in shared preferences.
+                editor.putString(EMAIL_KEY, findUser.getEmail());
+                editor.putString(PASSWORD_KEY, findUser.getPassword());
+
+                // to save our data with key and value.
+                editor.apply();
+
                 Intent myIntent = new Intent(MainActivity.this, Home.class);
                 startActivity(myIntent);
                 finish();
